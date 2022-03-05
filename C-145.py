52e155e686720913fa5c14f08237bb0458254b58
durga-555 <77527502+durga-555@users.noreply.github.com>
2022-03-05 21:04:55 +0530

    Update C-145.py

--- C-145.py
+++ C-145.py
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-      <script src = "https://aframe.io/releases/0.6.0/aframe.min.js"></script>
-    </head>
-    <body>
-      <a-scene background= "color: blue">
-      <a-box color= "purple" position = "-1 0.5 -3 "></a-box>
-        <a-sphere position = "0.25 1 -5" radius = "1.25" color = "green"></a-sphere>
-        <a-cyclinder position = "1 0.3 -6" radius = "1" color = "orange"></a-cyclinder>
-        <a-cone position = "0.75 1 -2" radius = 4 height = "2.5" color = "yellow"></a-cone> 
-        <a-plane position = "0 0 -4" rotation = "-90 0 0" width = "4" height = "4" color = "pink"></a-plane> 
-        <a-sky color = "grey"></a-sky>
-      </a-scene>
+<html>
+  <head>
+    <script src = "https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+  </head>
+  <body>
+    <a-scene background= "color: blue">
+    <a-box color= "purple" position = "-1 0.5 -3 "></a-box>
+      <a-sphere position = "0.25 1 -5" radius = "1.25" color = "green"></a-sphere>
+      <a-cyclinder position = "1 0.3 -6" radius = "1" color = "orange"></a-cyclinder>
+      <a-cone position = "0.75 1 -2" radius = 4 height = "2.5" color = "yellow"></a-cone> 
+      <a-plane position = "0 0 -4" rotation = "-90 0 0" width = "4" height = "4" color = "pink"></a-plane> 
+      <a-sky color = "grey"></a-sky>
+    </a-scene>
     
-    </body>
-  </html>
+  </body>
+</html>
